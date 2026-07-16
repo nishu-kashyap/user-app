ec725185fa37287e31cd01dfbae3dac6e7798f8f
nishu-kashyap <nishu@gharart.com>
2026-07-16 09:34:39 +0530

    Rename components back to jsx

---
+++ src/components/create_user.jsx
@@ -0,0 +1,9 @@
+function CreateUser() {
+    return (
+        <div>
+            <h1>Create User Page</h1>
+        </div>
+    );
+}
+
+export default CreateUser;
---
+++ src/components/user_list.jsx
@@ -0,0 +1,9 @@
+function UserList() {
+    return (
+        <div>
+            <h1>User List Page</h1>
+        </div>
+    );
+}
+
+export default UserList;
