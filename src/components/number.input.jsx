20b0f344c5b2540f7e564b8ee68e58ad54700f87
nishu-kashyap <nishu@gharart.com>
2026-07-17 09:48:56 +0530

    Restore input components

---
+++ src/components/number.input.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+
+
+function NumberInput({ label, successEvent }) {
+
+    const [value, setValue] = useState("");
+    const [error, setError] = useState("");
+
+
+    const validateNumber = (input) => {
+
+        // Empty check
+        if (!input) {
+            return "Age is required";
+        }
+
+
+        // Only number or starting negative number
+        const regex = /^-?\d+$/;
+
+
+        if (!regex.test(input)) {
+            return "Only numbers are allowed";
+        }
+
+
+        // Age validation
+        if (Number(input) < 0) {
+            return "Age cannot be negative";
+        }
+
+
+        if (Number(input) > 120) {
+            return "Enter valid age";
+        }
+
+
+        return "";
+
+    };
+
+
+    const handleChange = (e) => {
+
+        const input = e.target.value;
+
+        setValue(input);
+
+
+        const validationError = validateNumber(input);
+
+
+        if (validationError) {
+
+            setError(validationError);
+
+        }
+        else {
+
+            setError("");
+
+            successEvent(input);
+
+        }
+
+    };
+
+
+    return (
+
+        <div>
+
+            <label>{label}</label>
+
+            <input
+                type="text"
+                value={value}
+                onChange={handleChange}
+            />
+
+
+            {
+                error &&
+                <p style={{ color: "red" }}>
+                    {error}
+                </p>
+            }
+
+        </div>
+
+    );
+
+}
+
+
+export default NumberInput;
