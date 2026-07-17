ee15a1fa85276938fe53d46f6cff3d33fcfb0d54
nishu-kashyap <nishu@gharart.com>
2026-07-17 08:19:06 +0530

    Added email, password and age fields

--- src/components/create_user.jsx
+++ src/components/create_user.jsx
@@ -1,44 +1,29 @@
-import { useState } from "react";
-import EmailInput from "./email.input";
-import PasswordInput from "./pwd.input";
-
 function CreateUser() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [age, setAge] = useState("");
-
     return (
-        <div style={{ padding: "20px" }}>
+        <div>
             <h2>Create User</h2>
 
+            <div>
+                <label>Email</label>
+                <br />
+                <input type="email" placeholder="Enter Email" />
+            </div>
 
-            <EmailInput
-                successEvent={(value) => console.log("Valid Email:", value)}
-            />
             <br />
 
-            <PasswordInput
-                successEvent={(password) => {
-                    console.log("Password valid:", password)
-                }}
-            />
+            <div>
+                <label>Password</label>
+                <br />
+                <input type="password" placeholder="Enter Password" />
+            </div>
 
             <br />
 
             <div>
                 <label>Age</label>
                 <br />
-                <input
-                    type="number"
-                    placeholder="Enter Age"
-                    value={age}
-                    onChange={(e) => setAge(e.target.value)}
-                />
+                <input type="number" placeholder="Enter Age" />
             </div>
-
-            <br />
-
-            <button>Submit</button>
         </div>
     );
 }
